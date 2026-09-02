0f208d5206fb9445b9691e6b6b2bab2e4603ded1
Alex Gorischek <alex.gorischek@gmail.com>
2026-09-02 09:04:14 -0700

    Split instructions command implementation

---
+++ cmd/considered/instructions.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/agorischek/considered/internal/considered"
+)
+
+func runInstructions(args []string) int {
+	fs := flag.NewFlagSet("instructions", flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	root := fs.String("root", ".", "repository root")
+	configPath := fs.String("config", "", "configuration file")
+	if err := fs.Parse(args); err != nil {
+		return 2
+	}
+	cfg, err := considered.LoadConfig(resolveConfigPath(*root, *configPath))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 2
+	}
+	if cfg.Instructions == "" {
+		fmt.Fprintln(os.Stdout, "(No instructions provided)")
+		return 0
+	}
+	if err := writeInstructions(os.Stdout, cfg.Instructions, false); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 2
+	}
+	return 0
+}
+
+func writeInstructions(out io.Writer, instructions string, prefixed bool) error {
+	if prefixed {
+		if _, err := fmt.Fprint(out, "Instructions: "); err != nil {
+			return err
+		}
+	}
+	if _, err := fmt.Fprint(out, instructions); err != nil {
+		return err
+	}
+	if !strings.HasSuffix(instructions, "\n") {
+		if _, err := fmt.Fprintln(out); err != nil {
+			return err
+		}
+	}
+	if prefixed {
+		_, err := fmt.Fprintln(out)
+		return err
+	}
+	return nil
+}
--- cmd/considered/main.go
+++ cmd/considered/main.go
@@ -5,11 +5,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime/debug"
-	"strings"
 
 	"github.com/agorischek/considered/internal/considered"
 )
@@ -116,51 +114,6 @@ func runCheck(args []string) int {
 		return 1
 	}
 	return 0
-}
-
-func runInstructions(args []string) int {
-	fs := flag.NewFlagSet("instructions", flag.ContinueOnError)
-	fs.SetOutput(os.Stderr)
-	root := fs.String("root", ".", "repository root")
-	configPath := fs.String("config", "", "configuration file")
-	if err := fs.Parse(args); err != nil {
-		return 2
-	}
-	cfg, err := considered.LoadConfig(resolveConfigPath(*root, *configPath))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return 2
-	}
-	if cfg.Instructions == "" {
-		fmt.Fprintln(os.Stdout, "(No instructions provided)")
-		return 0
-	}
-	if err := writeInstructions(os.Stdout, cfg.Instructions, false); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return 2
-	}
-	return 0
-}
-
-func writeInstructions(out io.Writer, instructions string, prefixed bool) error {
-	if prefixed {
-		if _, err := fmt.Fprint(out, "Instructions: "); err != nil {
-			return err
-		}
-	}
-	if _, err := fmt.Fprint(out, instructions); err != nil {
-		return err
-	}
-	if !strings.HasSuffix(instructions, "\n") {
-		if _, err := fmt.Fprintln(out); err != nil {
-			return err
-		}
-	}
-	if prefixed {
-		_, err := fmt.Fprintln(out)
-		return err
-	}
-	return nil
 }
 
 func runVariance(args []string) int {
